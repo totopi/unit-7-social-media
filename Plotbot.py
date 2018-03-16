45f39ad9c1b4943dbbdabeca6b17748c5e2a0d53
Kevin McNalley <kevin.c.mcnalley@gmail.com>
2018-03-16 02:33:47 -0700

    Bug crush

--- Plotbot.py
+++ Plotbot.py
@@ -60,7 +60,7 @@ def the_work(analyzed=[], last_check=None, own_account="GunmaKuma"):
                 plt.legend(loc=(1,.95))
                 plt.xlabel("Tweets Ago")
                 plt.ylabel("Tweet Polarity")
-                plt.xlim(-len(df["Ago"]) - (len(df["Ago"]) * 0.01), (len(df["Ago"] * 0.01)))
+                plt.xlim(-len(df["Ago"]) - (len(df["Ago"]) * 0.01), (len(df["Ago"]) * 0.01))
                 plt.ylim(-1.01, 1.01)
                 plt.title(f"Sentiment Analysis of Tweets ({str_now})")
                 plt.savefig(f"{second_at}.png")
@@ -68,7 +68,7 @@ def the_work(analyzed=[], last_check=None, own_account="GunmaKuma"):
                 chirp.update_with_media(f"{second_at}.png", f"Tweet Analysis of @{second_at} (You're welcome, @{request_user})")
                 analyzed.append(second_at)
                 print("Tweeted, sir!")
-        except tweepy.TweepError:
+        except tweepy.TweepError as e:
             print("Something's up with twitter..." + e.reason)
         except:
             print("Nothing to do")
