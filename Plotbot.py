a26086c2f0bee491d825272f05080228626ed071
Kevin McNalley <kevin.c.mcnalley@gmail.com>
2018-03-16 01:39:00 -0700

    Plotbot deploy!

--- Plotbot.py
+++ Plotbot.py
@@ -25,8 +25,9 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import seaborn as sns
 import tweepy
 import time
